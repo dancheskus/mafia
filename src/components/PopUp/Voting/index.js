ca852d4d84b29c4ebf533092e6a852069e3c5d36
DESKTOP-89N3UKL\danch <dancheskus@gmail.com>
2018-12-24 19:34:28 +0200

    Добавлено много функционала и логики в голосование.

--- src/components/PopUp/Voting/index.js
+++ src/components/PopUp/Voting/index.js
@@ -6,15 +6,43 @@ import PopUpButton from '../style/PopUpButton';
 import HandsButton from './style/HandsButton';
 import Circle from './style/Circle';
 import BlockOfHands from './style/BlockOfHands';
+import { clearSelectedNumbers, addToSelectedNumbers } from '../../../redux/actions/gameActions';
+import Timer from '../../Timer';
 
 class Voting extends Component {
-  state = { handsAmount: Array(this.props.game.selectedNumbers.length).fill(0), handsLeft: 10, currentPlayer: 0 };
+  initialState = {
+    handsAmount: Array(this.props.game.selectedNumbers.length).fill(0),
+    handsLeft: 10,
+    currentPlayer: 0,
+    timer: false,
+    carCrash: 0,
+  };
+  state = { ...this.initialState };
 
   handClicked = num => {
+    if (this.state.currentPlayer === this.props.game.selectedNumbers.length - 1) return;
+
     const { currentPlayer, handsAmount } = this.state;
     const arr = [...handsAmount];
     arr[currentPlayer] = handsAmount[currentPlayer] === num ? null : num;
     this.setState({ handsAmount: arr });
+  };
+
+  votingFinishedClicked = () => {
+    const { handsAmount } = this.state;
+    let largestNumber = Math.max(...handsAmount);
+    const newVotingList = [];
+    handsAmount.filter((el, i) => el === largestNumber && newVotingList.push(this.props.game.selectedNumbers[i]));
+
+    if (newVotingList.length > 1) {
+      this.props.clearSelectedNumbers();
+      newVotingList.map(num => this.props.addToSelectedNumbers(num));
+      this.setState({ ...this.initialState, timer: true });
+
+      if (this.state.carCrash) this.setState({ carCrash: 2 });
+    }
+
+    this.state.timer && this.setState({ ...this.initialState, carCrash: 1 });
   };
 
   nextButtonClicked = () => {
@@ -35,34 +63,34 @@ class Voting extends Component {
 
   render = () => {
     const deadPlayers = this.props.players.filter(player => !player.isAlive).length;
+    const avaliableHandsAmount = this.state.handsAmount[this.props.game.selectedNumbers.length - 1] - deadPlayers;
+    const lastPlayer = this.state.currentPlayer === this.props.game.selectedNumbers.length - 1;
 
     return (
       <>
-        <Circle>{this.props.game.selectedNumbers[this.state.currentPlayer]}</Circle>
+        <Circle>{this.state.carCrash === 2 ? 'ВСЕ' : this.props.game.selectedNumbers[this.state.currentPlayer]}</Circle>
 
-        <BlockOfHands className="col-10 col-md-8 col-lg-6">
-          {_.range(1, 11).map(num => (
-            <HandsButton
-              disabled={
-                (this.state.currentPlayer === this.props.game.selectedNumbers.length - 1 &&
-                  num !== this.state.handsAmount[this.props.game.selectedNumbers.length - 1] - deadPlayers) ||
-                num > this.state.handsLeft - deadPlayers
-              }
-              selected={
-                this.state.currentPlayer === this.props.game.selectedNumbers.length - 1
-                  ? num === this.state.handsAmount[this.props.game.selectedNumbers.length - 1] - deadPlayers
-                  : this.state.handsAmount[this.state.currentPlayer] === num
-              }
-              onClick={() => this.handClicked(num)}
-              key={num}
-            >
-              <div className="number">{num}</div>
-            </HandsButton>
-          ))}
-        </BlockOfHands>
+        {this.state.timer && this.state.carCrash !== 2 ? (
+          <Timer time={30} key={this.state.currentPlayer} />
+        ) : (
+          <BlockOfHands className="col-10 col-md-8 col-lg-6">
+            {_.range(1, 11).map(num => (
+              <HandsButton
+                disabled={lastPlayer ? num !== avaliableHandsAmount : num > this.state.handsLeft - deadPlayers}
+                selected={
+                  lastPlayer ? num === avaliableHandsAmount : this.state.handsAmount[this.state.currentPlayer] === num
+                }
+                onClick={() => this.handClicked(num)}
+                key={num}
+              >
+                <div className="number">{num}</div>
+              </HandsButton>
+            ))}
+          </BlockOfHands>
+        )}
 
-        <PopUpButton color="Voting" onClick={this.nextButtonClicked}>
-          {this.state.currentPlayer < this.props.game.selectedNumbers.length - 1 ? 'Далее' : 'Завершить'}
+        <PopUpButton color="Voting" onClick={lastPlayer ? this.votingFinishedClicked : this.nextButtonClicked}>
+          {lastPlayer ? 'Завершить' : 'Далее'}
         </PopUpButton>
       </>
     );
@@ -71,7 +99,10 @@ class Voting extends Component {
 
 const mapStateToProps = ({ game, players }) => ({ game, players });
 
-const mapDispatchToProps = dispatch => ({});
+const mapDispatchToProps = dispatch => ({
+  clearSelectedNumbers: () => dispatch(clearSelectedNumbers()),
+  addToSelectedNumbers: playerNumber => dispatch(addToSelectedNumbers(playerNumber)),
+});
 
 export default connect(
   mapStateToProps,
