51fef33e651791e64eb428b0dd5030264ddf0b8e
DESKTOP-89N3UKL\danch <dancheskus@gmail.com>
2019-02-07 18:17:43 +0200

    Добавлен axios. И минимальный функционал для плеера. Play/pause/nextSong

--- src/components/AudioPlayer.js
+++ src/components/AudioPlayer.js
@@ -1,13 +1,38 @@
 import React, { Component } from 'react';
 import ReactPlayer from 'react-player';
+import axios from 'axios';
+
+const regex = /(?<=>).+(?=\.mp3)/g;
+
+const url = 'https://mafia-city.ml/music/';
 
 export default class AudioPlayer extends Component {
+  state = { volume: 1, isPlaying: false, songList: [], songName: '' };
+
+  getRandomSong = list => list[Math.floor(Math.random() * list.length)];
+
+  componentWillMount = () => {
+    axios.get('https://mafia-city.ml/music/').then(res => {
+      const songList = res.data.match(regex);
+      this.setState({ songList, songName: this.getRandomSong(songList) });
+    });
+  };
+
+  playPause = () => this.setState({ isPlaying: !this.state.isPlaying });
+  nextSong = () => this.setState({ songName: this.getRandomSong(this.state.songList) });
+
   render = () => (
-    <ReactPlayer
-      url="https://www.dropbox.com/s/03dlofceyawifnf/Podcast.mp3?dl=1"
-      controls
-      height="55px"
-      width="400px"
-    />
+    <>
+      <ReactPlayer
+        url={`${url}${this.state.songName}.mp3`}
+        playing={this.state.isPlaying}
+        // controls
+        height="55px"
+        width="400px"
+        volume={this.state.volume}
+      />
+      <button onClick={this.playPause}>{this.state.isPlaying ? 'PAUSE' : 'PLAY'}</button>
+      <button onClick={this.nextSong}>NEXT SONG</button>
+    </>
   );
 }
