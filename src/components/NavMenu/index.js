ac6eeaac871c4f41c4c11d0e4ec633d5aa5b9f59
DESKTOP-89N3UKL\danch <dancheskus@gmail.com>
2019-03-11 16:03:37 +0200

    NavMenu > index --> hooks

--- src/components/NavMenu/index.js
+++ src/components/NavMenu/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { connect } from 'react-redux';
 
 import { StyledNavMenu, MenuItems } from './style';
@@ -6,64 +6,66 @@ import { resetPlayersReducer } from 'redux/actions/playersActions';
 import { resetGameReducer } from 'redux/actions/gameActions';
 import Settings from './Settings';
 
-class NavMenu extends Component {
-  state = { checked: false, settingsPage: false };
+const NavMenu = props => {
+  const [isChecked, setIsChecked] = useState(false);
+  const [isSettingsPage, setIsSettingsPage] = useState(false);
 
-  render = () => {
-    const { tutorialEnabled } = this.props.settings;
+  const { tutorialEnabled } = props.settings;
 
-    return (
-      <StyledNavMenu tutorialEnabled={tutorialEnabled}>
-        <input
-          type="checkbox"
-          onChange={() => this.setState({ checked: !this.state.checked, settingsPage: false })}
-          // checked
-          checked={this.state.checked}
-          className="navi_check"
-          id="navi-toggle"
-        />
+  return (
+    <StyledNavMenu tutorialEnabled={tutorialEnabled}>
+      <input
+        type="checkbox"
+        onChange={() => {
+          setIsChecked(!isChecked);
+          setIsSettingsPage(false);
+        }}
+        // checked
+        checked={isChecked}
+        className="navi_check"
+        id="navi-toggle"
+      />
 
-        <label htmlFor="navi-toggle" className="navi_button">
-          <span className="navi_icon">&nbsp;</span>
-        </label>
+      <label htmlFor="navi-toggle" className="navi_button">
+        <span className="navi_icon">&nbsp;</span>
+      </label>
 
-        <div className="navi-background" />
+      <div className="navi-background" />
 
-        <Settings hide={!this.state.settingsPage} onClose={() => this.setState({ settingsPage: false })} />
+      <Settings hide={!isSettingsPage} onClose={() => setIsSettingsPage(false)} />
 
-        <MenuItems hide={this.state.settingsPage} className="navi_nav">
-          <ul className="navi_list">
-            <li onClick={() => this.setState({ settingsPage: true })} className="navi_item">
-              <div className="navi_link">Настройки</div>
-            </li>
+      <MenuItems hide={isSettingsPage} className="navi_nav">
+        <ul className="navi_list">
+          <li onClick={() => setIsSettingsPage(true)} className="navi_item">
+            <div className="navi_link">Настройки</div>
+          </li>
 
-            <li className="navi_item">
-              <div
-                onClick={() => {
-                  localStorage.clear();
-                  this.props.resetGameReducer();
-                  this.props.resetPlayersReducer();
-                  this.setState({ checked: false });
-                }}
-                className="navi_link"
-              >
-                Новая игра
-              </div>
-            </li>
-          </ul>
-        </MenuItems>
+          <li className="navi_item">
+            <div
+              onClick={() => {
+                localStorage.clear();
+                props.resetGameReducer();
+                props.resetPlayersReducer();
+                setIsChecked(false);
+              }}
+              className="navi_link"
+            >
+              Новая игра
+            </div>
+          </li>
+        </ul>
+      </MenuItems>
 
-        <div className="menu-footer">
-          Проект{' '}
-          <a target="_blank" rel="noopener noreferrer" href="https://github.com/dancheskus">
-            Даниэля Шлейфмана
-          </a>
-          . 2018-2019г.
-        </div>
-      </StyledNavMenu>
-    );
-  };
-}
+      <div className="menu-footer">
+        Проект{' '}
+        <a target="_blank" rel="noopener noreferrer" href="https://github.com/dancheskus">
+          Даниэля Шлейфмана
+        </a>
+        . 2018-2019г.
+      </div>
+    </StyledNavMenu>
+  );
+};
 
 export default connect(
   ({ game, settings }) => ({ game, settings }),
