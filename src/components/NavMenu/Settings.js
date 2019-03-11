e3a18695c74caaec1de971db7e2a7ff7a993bf28
DESKTOP-89N3UKL\danch <dancheskus@gmail.com>
2019-03-11 16:06:46 +0200

    Settings stateles component

--- src/components/NavMenu/Settings.js
+++ src/components/NavMenu/Settings.js
@@ -7,33 +7,31 @@ import { enableTutorial } from 'redux/actions/settingsActions';
 import { resetGameReducer } from 'redux/actions/gameActions';
 import { resetPlayersReducer } from 'redux/actions/playersActions';
 
-class Settings extends Component {
-  enableTutorial = () => {
-    this.props.resetGameReducer();
-    this.props.resetPlayersReducer();
-    this.props.enableTutorial();
+const Settings = props => {
+  const enableTutorial = () => {
+    props.resetGameReducer();
+    props.resetPlayersReducer();
+    props.enableTutorial();
   };
 
-  render = () => {
-    const { tutorialEnabled } = this.props.settings;
+  const { tutorialEnabled } = props.settings;
 
-    return (
-      <AppSettings hide={this.props.hide} tutorialEnabled={tutorialEnabled}>
-        <BackButton onClick={this.props.onClose}>
-          <NextIcon />
-        </BackButton>
+  return (
+    <AppSettings hide={props.hide} tutorialEnabled={tutorialEnabled}>
+      <BackButton onClick={props.onClose}>
+        <NextIcon />
+      </BackButton>
 
-        <SettingsItem title="Раздача номеров в начале игры" type="seatAllocator" />
-        <SettingsItem title="Музыка" type="appMusic" />
-        <SettingsItem title="Звуки таймера (на 15/5 сек)" type="timerSounds" />
-        <SettingsItem title="Таймер на договорку (1 мин)" type="mafiaTimer" />
-        <SettingsItem title="Предлагать вывести всех после повторной переголосовки" type="multiplePlayerRemove" />
+      <SettingsItem title="Раздача номеров в начале игры" type="seatAllocator" />
+      <SettingsItem title="Музыка" type="appMusic" />
+      <SettingsItem title="Звуки таймера (на 15/5 сек)" type="timerSounds" />
+      <SettingsItem title="Таймер на договорку (1 мин)" type="mafiaTimer" />
+      <SettingsItem title="Предлагать вывести всех после повторной переголосовки" type="multiplePlayerRemove" />
 
-        <RepeatGuideButton onClick={this.enableTutorial}>Включить обучение</RepeatGuideButton>
-      </AppSettings>
-    );
-  };
-}
+      <RepeatGuideButton onClick={enableTutorial}>Включить обучение</RepeatGuideButton>
+    </AppSettings>
+  );
+};
 
 export default connect(
   ({ settings }) => ({ settings }),
