01af32cfd5e40e5cecbfb29d74bf173c92d3a48c
DESKTOP-89N3UKL\danch <dancheskus@gmail.com>
2019-04-22 15:02:20 +0300

    Добавлен диалог о начале новой игры

--- src/components/NavMenu/index.js
+++ src/components/NavMenu/index.js
@@ -11,54 +11,59 @@ const NavMenu = props => {
   const [isSettingsPage, setIsSettingsPage] = useState(false);
 
   const { tutorialEnabled } = props.settings;
+  const { phase } = props.game.gameState;
+  const askNewGameConfirmation = phase !== 'SeatAllocator' && phase !== 'EndOfGame';
+
+  const newGameClicked = () => {
+    const startNewGame = () => {
+      localStorage.clear();
+      props.resetGameReducer();
+      props.resetPlayersReducer();
+      setIsChecked(false);
+    };
+
+    askNewGameConfirmation ? window.confirm('Начать новую игру?') && startNewGame() : startNewGame();
+  };
 
   return (
     <StyledNavMenu tutorialEnabled={tutorialEnabled}>
       <input
-        type="checkbox"
+        type='checkbox'
         onChange={() => {
           setIsChecked(!isChecked);
           setIsSettingsPage(false);
         }}
         // checked
         checked={isChecked}
-        className="navi_check"
-        id="navi-toggle"
+        className='navi_check'
+        id='navi-toggle'
       />
 
-      <label htmlFor="navi-toggle" className="navi_button">
-        <span className="navi_icon">&nbsp;</span>
+      <label htmlFor='navi-toggle' className='navi_button'>
+        <span className='navi_icon'>&nbsp;</span>
       </label>
 
-      <div className="navi-background" />
+      <div className='navi-background' />
 
       <Settings hide={!isSettingsPage} onClose={() => setIsSettingsPage(false)} />
 
-      <MenuItems hide={isSettingsPage} className="navi_nav">
-        <ul className="navi_list">
-          <li onClick={() => setIsSettingsPage(true)} className="navi_item">
-            <div className="navi_link">Настройки</div>
+      <MenuItems hide={isSettingsPage} className='navi_nav'>
+        <ul className='navi_list'>
+          <li onClick={() => setIsSettingsPage(true)} className='navi_item'>
+            <div className='navi_link'>Настройки</div>
           </li>
 
-          <li className="navi_item">
-            <div
-              onClick={() => {
-                localStorage.clear();
-                props.resetGameReducer();
-                props.resetPlayersReducer();
-                setIsChecked(false);
-              }}
-              className="navi_link"
-            >
+          <li className='navi_item'>
+            <div onClick={newGameClicked} className='navi_link'>
               Новая игра
             </div>
           </li>
         </ul>
       </MenuItems>
 
-      <div className="menu-footer">
+      <div className='menu-footer'>
         Проект{' '}
-        <a target="_blank" rel="noopener noreferrer" href="https://github.com/dancheskus">
+        <a target='_blank' rel='noopener noreferrer' href='https://github.com/dancheskus'>
           Даниэля Шлейфмана
         </a>
         . 2018-2019г.
