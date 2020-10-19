43d1ab1d05d0ab2df7cf56846e4892fee7d70327
Daniel Shleifman <dancheskus@gmail.com>
2020-10-19 18:30:18 +0300

    refactro

--- src/components/UserGuide/index.js
+++ src/components/UserGuide/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+
 import { disableTutorial } from 'redux/actions/settingsActions';
 import {
   resetGameReducer,
@@ -8,6 +9,7 @@ import {
   changeActivePlayer,
 } from 'redux/actions/gameActions';
 import { resetPlayersReducer, addRole } from 'redux/actions/playersActions';
+
 import {
   GuideWrapperStyle,
   GuideStepWrapperStyle,
@@ -21,61 +23,66 @@ class UsersGuide extends Component {
   state = { currentStep: 0 };
 
   componentDidMount = () => {
-    if (!this.props.settings.tutorialEnabled) return;
-
-    this.props.changeActivePlayer(0);
-
     const {
-      gameState: { phase },
-      popupMinimized,
-    } = this.props.game;
+      game: {
+        gameState: { phase },
+        popupMinimized,
+      },
+      settings,
+      changeActivePlayer,
+      minimizeMaximaizePopup,
+    } = this.props;
 
-    popupMinimized && this.props.minimizeMaximaizePopup();
+    if (!settings.tutorialEnabled) return;
 
-    if (phase === 'RoleDealing' || !this.props.settings.seatAllocator) this.setState({ currentStep: 3 });
+    changeActivePlayer(0);
+
+    popupMinimized && minimizeMaximaizePopup();
+
+    if (phase === 'RoleDealing' || !settings.seatAllocator) this.setState({ currentStep: 3 });
     if (phase === 'ZeroNight') this.setState({ currentStep: 5 });
     if (phase === 'Day') this.setState({ currentStep: 7 });
   };
 
   componentDidUpdate = (_, prevState) => {
-    if (!this.props.settings.tutorialEnabled) return;
-
-    const { currentStep } = this.state;
-    const { changeGameState } = this.props;
     const {
-      popupMinimized,
-      activePlayer,
-      gameState: { phase },
-    } = this.props.game;
-    const { appMusic } = this.props.settings;
+      changeGameState,
+      settings: { appMusic, tutorialEnabled, seatAllocator },
+      game: {
+        popupMinimized,
+        activePlayer,
+        gameState: { phase },
+      },
+      addRole,
+      minimizeMaximaizePopup,
+    } = this.props;
+    const { currentStep } = this.state;
+
+    if (!tutorialEnabled) return;
+
     const stepChanged = prevState.currentStep !== currentStep;
 
-    if ((phase === 'RoleDealing' || !this.props.settings.seatAllocator) && currentStep === 0)
-      this.setState({ currentStep: 3 });
+    if ((phase === 'RoleDealing' || !seatAllocator) && currentStep === 0) this.setState({ currentStep: 3 });
 
     if (stepChanged) {
       if (currentStep === 3) changeGameState({ phase: 'RoleDealing' });
       if (currentStep === 5) {
         changeGameState({ phase: 'ZeroNight' });
-        this.props.addRole({ playerNumber: 1, role: 'МАФИЯ' });
-        this.props.addRole({ playerNumber: 2, role: 'МАФИЯ' });
-        this.props.addRole({ playerNumber: 3, role: 'ДОН' });
-        this.props.addRole({ playerNumber: 4, role: 'ШЕРИФ' });
+        addRole({ playerNumber: 1, role: 'МАФИЯ' });
+        addRole({ playerNumber: 2, role: 'МАФИЯ' });
+        addRole({ playerNumber: 3, role: 'ДОН' });
+        addRole({ playerNumber: 4, role: 'ШЕРИФ' });
 
-        for (let i = 5; i <= 9; i++) {
-          this.props.addRole({ playerNumber: i, role: 'МИРНЫЙ' });
-        }
+        for (let i = 5; i <= 9; i++) addRole({ playerNumber: i, role: 'МИРНЫЙ' });
       }
-      if (currentStep === 7) {
-        changeGameState({ phase: 'Day', dayNumber: 1 });
-      }
+      if (currentStep === 7) changeGameState({ phase: 'Day', dayNumber: 1 });
     }
 
     if (!appMusic && currentStep === 3) this.setState({ currentStep: 4 });
 
     if (currentStep < 7) {
       if (popupMinimized && currentStep !== 6) this.setState({ currentStep: 6 });
-      if (!popupMinimized && currentStep === 6) this.props.minimizeMaximaizePopup();
+      if (!popupMinimized && currentStep === 6) minimizeMaximaizePopup();
     }
 
     if (activePlayer === 3 && currentStep !== 8) this.setState({ currentStep: 8 });
@@ -84,41 +91,55 @@ class UsersGuide extends Component {
   nextStep = () => this.setState({ currentStep: this.state.currentStep + 1 });
 
   endGuide = () => {
-    this.props.game.popupMinimized && this.props.minimizeMaximaizePopup();
+    const {
+      game: { popupMinimized },
+      minimizeMaximaizePopup,
+      disableTutorial,
+      resetGameReducer,
+      resetPlayersReducer,
+    } = this.props;
+
+    popupMinimized && minimizeMaximaizePopup();
     this.setState({ currentStep: 0 });
-    this.props.disableTutorial();
-    this.props.resetGameReducer();
-    this.props.resetPlayersReducer();
+    disableTutorial();
+    resetGameReducer();
+    resetPlayersReducer();
   };
 
   splitLines = text => text.split('\n').map(i => <p key={i}>{i}</p>);
 
   render = () => {
-    const { tutorialEnabled } = this.props.settings;
+    const {
+      children,
+      settings: { tutorialEnabled },
+    } = this.props;
     const { currentStep } = this.state;
     const guideStepsAvaliable = currentStep < stepDescription.length - 1;
 
     return tutorialEnabled ? (
-      <GuideWrapperStyle currentStep={this.state.currentStep}>
+      <GuideWrapperStyle currentStep={currentStep}>
         <GuideStepWrapperStyle position={stepDescription[currentStep].position}>
           {stepDescription[currentStep].title && (
             <GuideStepTitleStyle>{stepDescription[currentStep].title}</GuideStepTitleStyle>
           )}
+
           <GuideStepContentStyle>{this.splitLines(stepDescription[currentStep].content)}</GuideStepContentStyle>
         </GuideStepWrapperStyle>
 
         <GuideButton guideNextStep onClick={guideStepsAvaliable ? this.nextStep : this.endGuide}>
           {guideStepsAvaliable ? 'Далее' : 'Завершить'}
         </GuideButton>
+
         {guideStepsAvaliable && (
           <GuideButton skipGuide onClick={this.endGuide}>
             Завершить
           </GuideButton>
         )}
-        {this.props.children}
+
+        {children}
       </GuideWrapperStyle>
     ) : (
-      this.props.children
+      children
     );
   };
 }
