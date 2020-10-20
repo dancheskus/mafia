946519925c8317e70a5afa3f21dbd9a7d0e4a35b
Daniel Shleifman <dancheskus@gmail.com>
2020-10-20 17:13:56 +0300

    SingleCard rewritten with hooks

--- src/components/PlayerCards/SingleCard.js
+++ src/components/PlayerCards/SingleCard.js
@@ -1,5 +1,6 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useTimer } from 'use-timer';
 
 import { MinimizeIcon, MaximizeIcon, NextIcon } from 'icons/svgIcons';
 import { addFoul, removeFoul, returnPlayerToGame } from 'redux/actions/playersActions';
@@ -8,143 +9,118 @@ import checkForEnd from 'helpers/checkForEnd';
 
 import { FoulContainer, PlayerNumber, CardContainer, Card, RemoveFoul, AddFoul, FoulIcon, BackButton } from './style';
 
-class SingleCard extends Component {
-  state = { foulsAmount: this.props.players[this.props.playerNumber].fouls.amount, lastFoulDeath: false };
+export default ({ order, playerNumber }) => {
+  const dispatch = useDispatch();
+  const {
+    players,
+    game: {
+      activePlayer,
+      opensTable,
+      gameState: { phase },
+    },
+  } = useSelector(store => store);
 
-  timer;
+  const {
+    isAlive,
+    role,
+    fouls: { muted, amount },
+  } = players[playerNumber];
 
-  returnToLifeTimer;
+  const [foulsAmount, setFoulsAmount] = useState(amount);
+  const [lastFoulDeath, setLastFoulDeath] = useState(false);
 
-  componentWillUnmount = () => {
-    clearTimeout(this.timer);
-    clearTimeout(this.returnToLifeTimer);
-  };
+  const returnToLifeTimer = useTimer({
+    initialTime: 1,
+    endTime: 0,
+    timerType: 'DECREMENTAL',
+    interval: 5000,
+    onTimeOver: () => setLastFoulDeath(false),
+  });
 
-  addFoul = () => {
-    const {
-      players,
-      addFoul,
-      skipVotingInc,
-      playerNumber,
-      changeGameState,
-      game: {
-        gameState: { phase },
-      },
-    } = this.props;
-    const { foulsAmount } = this.state;
+  const addFoulTimer = useTimer({
+    initialTime: 1,
+    endTime: 0,
+    timerType: 'DECREMENTAL',
+    interval: 2000,
+    onTimeOver: () => {
+      dispatch(addFoul(playerNumber));
 
-    if (foulsAmount === 4) return;
-
-    this.setState({ foulsAmount: foulsAmount + 1 });
-
-    if (foulsAmount !== 3) return addFoul(playerNumber);
-
-    this.timer = setTimeout(() => {
-      addFoul(playerNumber);
-
-      if (checkForEnd(players).status) return changeGameState({ phase: 'EndOfGame' });
+      if (checkForEnd(players).status) return dispatch(changeGameState({ phase: 'EndOfGame' }));
 
       if (phase === 'Day') {
-        this.setState({ lastFoulDeath: true });
-
-        this.returnToLifeTimer = setTimeout(() => {
-          this.setState({ lastFoulDeath: false });
-        }, 5000);
+        setLastFoulDeath(true);
+        returnToLifeTimer.start();
       }
 
-      skipVotingInc();
-    }, 2000);
+      dispatch(skipVotingInc());
+    },
+  });
+
+  const addFoulClicked = () => {
+    if (foulsAmount === 4) return;
+
+    setFoulsAmount(foulsAmount + 1);
+    if (foulsAmount !== 3) return dispatch(addFoul(playerNumber));
+    addFoulTimer.start();
   };
 
-  removeFoul = () => {
-    const { removeFoul, playerNumber } = this.props;
-    const { foulsAmount } = this.state;
-
+  const removeFoulClicked = () => {
     if (foulsAmount === 0) return;
-    this.setState({ foulsAmount: foulsAmount - 1 });
+
+    setFoulsAmount(foulsAmount - 1);
+
     if (foulsAmount === 4) {
-      clearTimeout(this.timer);
+      addFoulTimer.reset();
     } else {
-      removeFoul(playerNumber);
+      dispatch(removeFoul(playerNumber));
     }
   };
 
-  backToLife = () => {
-    const { returnPlayerToGame, playerNumber, skipVotingDec } = this.props;
-
-    clearTimeout(this.returnToLifeTimer);
-    returnPlayerToGame(playerNumber);
-    skipVotingDec();
-    this.setState({ foulsAmount: 3, lastFoulDeath: false });
+  const backToLife = () => {
+    returnToLifeTimer.reset();
+    dispatch(returnPlayerToGame(playerNumber));
+    dispatch(skipVotingDec());
+    setFoulsAmount(3);
+    setLastFoulDeath(false);
   };
 
-  render = () => {
-    const { backToLife, removeFoul, addFoul } = this;
-    const { lastFoulDeath, foulsAmount } = this.state;
-    const {
-      order,
-      playerNumber,
-      players,
-      game: {
-        activePlayer,
-        opensTable,
-        gameState: { phase },
-      },
-    } = this.props;
+  return (
+    <CardContainer order={order}>
+      <Card isAlive={isAlive} activePlayer={phase === 'Day' && activePlayer === playerNumber}>
+        <PlayerNumber
+          darkSide={role === 'МАФИЯ' || role === 'ДОН'}
+          role={role}
+          isMuted={muted}
+          isAlive={isAlive}
+          opensTable={phase === 'Day' && opensTable === playerNumber}
+        >
+          {lastFoulDeath && (
+            <BackButton onClick={backToLife}>
+              <NextIcon fill='lightgrey' />
+            </BackButton>
+          )}
 
-    const {
-      isAlive,
-      role,
-      fouls: { muted },
-    } = players[playerNumber];
+          <div className='number'>{playerNumber + 1}</div>
+        </PlayerNumber>
 
-    return (
-      <CardContainer order={order}>
-        <Card isAlive={isAlive} activePlayer={phase === 'Day' && activePlayer === playerNumber}>
-          <PlayerNumber
-            darkSide={role === 'МАФИЯ' || role === 'ДОН'}
-            role={role}
-            isMuted={muted}
-            isAlive={isAlive}
-            opensTable={phase === 'Day' && opensTable === playerNumber}
-          >
-            {lastFoulDeath && (
-              <BackButton onClick={backToLife}>
-                <NextIcon fill='lightgrey' />
-              </BackButton>
-            )}
+        <FoulContainer isAlive={isAlive}>
+          <RemoveFoul onClick={removeFoulClicked} className={playerNumber === 7 && 'remove-foul'}>
+            <FoulIcon remove>
+              <MinimizeIcon size='50%' />
+            </FoulIcon>
+          </RemoveFoul>
 
-            <div className='number'>{playerNumber + 1}</div>
-          </PlayerNumber>
-
-          <FoulContainer isAlive={isAlive}>
-            <RemoveFoul onClick={removeFoul} className={playerNumber === 7 && 'remove-foul'}>
-              <FoulIcon remove>
-                <MinimizeIcon size='50%' />
+          <AddFoul amount={foulsAmount} onClick={addFoulClicked} className={playerNumber === 7 && 'add-foul'}>
+            {foulsAmount ? (
+              '!'.repeat(foulsAmount)
+            ) : (
+              <FoulIcon>
+                <MaximizeIcon size='50%' />
               </FoulIcon>
-            </RemoveFoul>
-
-            <AddFoul amount={foulsAmount} onClick={addFoul} className={playerNumber === 7 && 'add-foul'}>
-              {foulsAmount ? (
-                '!'.repeat(foulsAmount)
-              ) : (
-                <FoulIcon>
-                  <MaximizeIcon size='50%' />
-                </FoulIcon>
-              )}
-            </AddFoul>
-          </FoulContainer>
-        </Card>
-      </CardContainer>
-    );
-  };
-}
-
-export default connect(({ game, players }) => ({ game, players }), {
-  addFoul,
-  removeFoul,
-  changeGameState,
-  skipVotingInc,
-  skipVotingDec,
-  returnPlayerToGame,
-})(SingleCard);
+            )}
+          </AddFoul>
+        </FoulContainer>
+      </Card>
+    </CardContainer>
+  );
+};
