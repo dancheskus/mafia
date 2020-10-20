f1db430b02f47c3994f3dfb313e78d46428323eb
Daniel Shleifman <dancheskus@gmail.com>
2020-10-20 18:24:41 +0300

    NavBar rewritten with hooks

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Container } from 'reactstrap';
-import { connect } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { countBy } from 'lodash';
 
 import NavMenu from 'components/NavMenu';
@@ -9,145 +9,127 @@ import { unmutePlayer } from 'redux/actions/playersActions';
 import { NextIcon, ThumbUpIcon, EyeIcon } from 'icons/svgIcons';
 import { disableTutorial } from 'redux/actions/settingsActions';
 import NavBarCircleButton from 'components/styled-components/NavBarCircleButton';
+import usePreviousState from 'helpers/usePreviousState';
+import Timer from 'components/Timer';
 
-import Timer from '../Timer';
 import AudioPlayer from './AudioPlayer';
 import { BackIcon, ButtonsWrapper, NavStateName, StyledNavigation } from './style';
 
 const mod = (n, m) => ((n % m) + m) % m;
 
-class Navigation extends Component {
-  state = { stepBackAvaliable: false };
+export default () => {
+  const dispatch = useDispatch();
+  const {
+    players,
+    game: {
+      gameState: { phase, dayNumber },
+      activePlayer,
+      opensTable,
+      selectedNumbers,
+    },
+    settings: { tutorialEnabled, appMusic },
+  } = useSelector(store => store);
 
-  componentDidUpdate = prevState => {
-    const { stepBackAvaliable } = this.state;
-    const { players } = this.props;
+  const [stepBackAvaliable, setStepBackAvaliable] = useState(false);
 
-    if (prevState.game.gameState.phase !== 'Day' && stepBackAvaliable) this.setState({ stepBackAvaliable: false });
+  const prevPhaseState = usePreviousState(phase);
+  const prevPlayersState = usePreviousState(players);
+
+  useEffect(() => {
+    if (prevPhaseState !== 'Day' && stepBackAvaliable) setStepBackAvaliable(false);
 
     // Если стало меньше живых игроков, выключить кнопку "назад"
-    const prevAllAlivePlayers = countBy(prevState.players.map(player => player.isAlive)).true;
-    const allAlivePlayers = countBy(players.map(player => player.isAlive)).true;
-    if (prevAllAlivePlayers !== allAlivePlayers) this.setState({ stepBackAvaliable: false });
+    const prevAllAlivePlayers = countBy(prevPlayersState?.map(({ isAlive }) => isAlive)).true;
+    const allAlivePlayers = countBy(players.map(({ isAlive }) => isAlive)).true;
+    if (prevAllAlivePlayers !== allAlivePlayers) setStepBackAvaliable(false);
+  });
+
+  const goToNextAlivePlayer = (i = activePlayer + 1) => {
+    dispatch(unmutePlayer(mod(i - 1, 10)));
+    players[mod(i, 10)].isAlive ? dispatch(changeActivePlayer(mod(i, 10))) : goToNextAlivePlayer(i + 1);
+    setStepBackAvaliable(true);
   };
 
-  goToNextAlivePlayer = (i = this.props.game.activePlayer + 1) => {
-    const { unmutePlayer, players, changeActivePlayer } = this.props;
-    unmutePlayer(mod(i - 1, 10));
+  const findLastSpeaker = (i = opensTable - 1) => (players[mod(i, 10)].isAlive ? mod(i, 10) : findLastSpeaker(i - 1));
 
-    players[mod(i, 10)].isAlive ? changeActivePlayer(mod(i, 10)) : this.goToNextAlivePlayer(i + 1);
-
-    this.setState({ stepBackAvaliable: true });
+  const goToPreviousAlivePlayer = (i = activePlayer - 1) => {
+    players[mod(i, 10)].isAlive ? dispatch(changeActivePlayer(mod(i, 10))) : goToPreviousAlivePlayer(i - 1);
+    setStepBackAvaliable(false);
   };
 
-  findLastSpeaker = (i = this.props.game.opensTable - 1) =>
-    this.props.players[mod(i, 10)].isAlive ? mod(i, 10) : this.findLastSpeaker(i - 1);
+  const toVotingClicked = () => {
+    dispatch(disableTutorial());
 
-  goToPreviousAlivePlayer = (i = this.props.game.activePlayer - 1) => {
-    const { players, changeActivePlayer } = this.props;
+    if (selectedNumbers.length) return dispatch(changeGameState({ phase: 'Voting' }));
 
-    players[mod(i, 10)].isAlive ? changeActivePlayer(mod(i, 10)) : this.goToPreviousAlivePlayer(i - 1);
-
-    this.setState({ stepBackAvaliable: false });
+    dispatch(skipVotingDec());
+    dispatch(changeGameState({ phase: 'Night' }));
   };
 
-  toVotingClicked = () => {
-    const { disableTutorial, changeGameState, skipVotingDec, game } = this.props;
-
-    disableTutorial();
-
-    if (game.selectedNumbers.length) return changeGameState({ phase: 'Voting' });
-
-    skipVotingDec();
-    changeGameState({ phase: 'Night' });
+  const phaseTitles = {
+    SeatAllocator: 'раздача номеров',
+    RoleDealing: 'раздача ролей',
+    ZeroNight: '0 ночь',
+    Day: `${dayNumber} день`,
+    Voting: 'Голосование',
+    Night: `${dayNumber} Ночь`,
+    EndOfGame: `Конец игры`,
   };
+  const currentPhaseTitle = phaseTitles[phase];
 
-  render = () => {
-    const {
-      players,
-      game: {
-        gameState: { phase, dayNumber },
-        activePlayer,
-        opensTable,
-        selectedNumbers,
-      },
-      settings: { tutorialEnabled, appMusic },
-    } = this.props;
-    const { stepBackAvaliable } = this.state;
+  const lastSpeaker = activePlayer === findLastSpeaker();
 
-    const phaseTitles = {
-      SeatAllocator: 'раздача номеров',
-      RoleDealing: 'раздача ролей',
-      ZeroNight: '0 ночь',
-      Day: `${dayNumber} день`,
-      Voting: 'Голосование',
-      Night: `${dayNumber} Ночь`,
-      EndOfGame: `Конец игры`,
-    };
-    const currentPhaseTitle = phaseTitles[phase];
+  const alivePlayers = players.filter(x => x.isAlive).length;
 
-    const lastSpeaker = activePlayer === this.findLastSpeaker();
+  return (
+    <StyledNavigation color={phase} tutorialEnabled={tutorialEnabled}>
+      <Container className='d-flex justify-content-between p-0'>
+        <NavStateName tutorialEnabled={tutorialEnabled} key={currentPhaseTitle}>
+          <span>{currentPhaseTitle}</span>
+        </NavStateName>
 
-    const alivePlayers = players.filter(x => x.isAlive).length;
-
-    return (
-      <StyledNavigation color={phase} tutorialEnabled={tutorialEnabled}>
-        <Container className='d-flex justify-content-between p-0'>
-          <NavStateName tutorialEnabled={tutorialEnabled} key={currentPhaseTitle}>
-            <span>{currentPhaseTitle}</span>
-          </NavStateName>
-
-          {phase === 'Day' && (
-            <ButtonsWrapper className='day-user-navigation'>
-              <NavBarCircleButton
-                disabled={!stepBackAvaliable}
-                onClick={() => (stepBackAvaliable ? this.goToPreviousAlivePlayer() : null)}
-              >
-                <BackIcon size='50%' />
-              </NavBarCircleButton>
-
-              <Timer
-                time={players[activePlayer].fouls.muted && (alivePlayers === 3 || alivePlayers === 4 ? 30 : 0)}
-                autostart={activePlayer !== opensTable}
-                mini
-                key={activePlayer}
-              />
-
-              <NavBarCircleButton onClick={lastSpeaker ? this.toVotingClicked : () => this.goToNextAlivePlayer()}>
-                {lastSpeaker ? (
-                  selectedNumbers.length === 0 ? (
-                    <EyeIcon size='50%' />
-                  ) : (
-                    <ThumbUpIcon size='50%' />
-                  )
-                ) : (
-                  <NextIcon size='50%' />
-                )}
-              </NavBarCircleButton>
-            </ButtonsWrapper>
-          )}
-
-          {appMusic && (
-            <ButtonsWrapper
-              style={{
-                ...(phase !== 'Night' && phase !== 'ZeroNight' && phase !== 'RoleDealing' && { display: 'none' }),
-              }}
+        {phase === 'Day' && (
+          <ButtonsWrapper className='day-user-navigation'>
+            <NavBarCircleButton
+              disabled={!stepBackAvaliable}
+              onClick={() => (stepBackAvaliable ? goToPreviousAlivePlayer() : null)}
             >
-              {phase !== 'SeatAllocator' && <AudioPlayer />}
-            </ButtonsWrapper>
-          )}
+              <BackIcon size='50%' />
+            </NavBarCircleButton>
 
-          <NavMenu />
-        </Container>
-      </StyledNavigation>
-    );
-  };
-}
+            <Timer
+              time={players[activePlayer].fouls.muted && (alivePlayers === 3 || alivePlayers === 4 ? 30 : 0)}
+              autostart={activePlayer !== opensTable}
+              mini
+              key={activePlayer}
+            />
 
-export default connect(({ game, players, settings }) => ({ game, players, settings }), {
-  changeActivePlayer,
-  changeGameState,
-  unmutePlayer,
-  skipVotingDec,
-  disableTutorial,
-})(Navigation);
+            <NavBarCircleButton onClick={lastSpeaker ? toVotingClicked : () => goToNextAlivePlayer()}>
+              {lastSpeaker ? (
+                selectedNumbers.length === 0 ? (
+                  <EyeIcon size='50%' />
+                ) : (
+                  <ThumbUpIcon size='50%' />
+                )
+              ) : (
+                <NextIcon size='50%' />
+              )}
+            </NavBarCircleButton>
+          </ButtonsWrapper>
+        )}
+
+        {appMusic && (
+          <ButtonsWrapper
+            style={{
+              ...(phase !== 'Night' && phase !== 'ZeroNight' && phase !== 'RoleDealing' && { display: 'none' }),
+            }}
+          >
+            {phase !== 'SeatAllocator' && <AudioPlayer />}
+          </ButtonsWrapper>
+        )}
+
+        <NavMenu />
+      </Container>
+    </StyledNavigation>
+  );
+};
