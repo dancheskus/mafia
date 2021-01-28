b7a4ab76da579dd402509b052dd034ee757f036d
Daniel Shleifman <dancheskus@gmail.com>
2021-01-28 16:59:06 +0200

    docker experiment

--- docker-scripts.sh
+++ docker-scripts.sh
@@ -2,8 +2,8 @@
 
 FOLDER=docker
 
-if [ "$1" = "stop" ]; then docker-compose -f ${FOLDER}/docker-compose.yml -f ${FOLDER}/prod.yml down
-elif [ "$1" = "prod" ]; then docker-compose -f ${FOLDER}/docker-compose.yml -f ${FOLDER}/prod.yml up $2
-elif [ "$1" = "prod:build" ]; then docker-compose -f ${FOLDER}/docker-compose.yml -f ${FOLDER}/prod.yml up --build $2
-else docker-compose -f ${FOLDER}/docker-compose.yml -f ${FOLDER}/dev.yml up
+if [ "$1" = "stop" ]; then docker-compose --project-directory . -f ${FOLDER}/docker-compose.yml -f ${FOLDER}/prod.yml down
+elif [ "$1" = "prod" ]; then docker-compose --project-directory . -f ${FOLDER}/docker-compose.yml -f ${FOLDER}/prod.yml up $2
+elif [ "$1" = "prod:build" ]; then docker-compose --project-directory . -f ${FOLDER}/docker-compose.yml -f ${FOLDER}/prod.yml up --build $2
+else docker-compose --project-directory . -f ${FOLDER}/docker-compose.yml -f ${FOLDER}/dev.yml up
 fi
